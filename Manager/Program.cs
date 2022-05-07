995b14ac3df7fd0c30b718d92a1abe7532d11f5b
abdallah nader <abdallahnader20@hotmail.com>
2022-05-07 10:40:41 +0300

    update3

--- Manager/Program.cs
+++ Manager/Program.cs
@@ -21,10 +21,10 @@ app.UseStaticFiles();
 
 //app.UseMvcWithDefaultRoute();
 
-//app.UseMvc(route =>
-//{
-//    route.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
-//});
+app.UseMvc(routes =>
+{
+    routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+});
 
 //app.MapGet("/", () => "Hello World!");
 
